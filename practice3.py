942b10c4bf77519a6f61dc703bc33a0fc84b6144
yerbol <93880074+aruaqsyz@users.noreply.github.com>
2022-09-11 15:51:42 +0600

    Update practice3.py

--- practice3.py
+++ practice3.py
@@ -1,3 +1,20 @@
+# exercise
+a = int(input("first number: "))
+b = int(input("second number: "))
+c = int(input("third number: "))
+
+if a < b:
+    if a < c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b < c:
+        print(b)
+    else:
+        print(c)
+
+
 # task 1
 def is_alive(health):
     if health <= 0:
